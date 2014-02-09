3ae46fd343df6d9fb69d0954b4db07b6b00c7303
Daniel Vollans <dan.vollans@gmail.com>
2014-02-08 20:02:19 -0700

    styling changes

--- media_automater/settings.py
+++ media_automater/settings.py
@@ -27,7 +27,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'server-main', '192.168.1.76']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
