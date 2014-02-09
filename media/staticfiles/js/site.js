5bc5998dce5f61cba1dbe1b7e191473938bb2eda
Daniel Vollans <dan.vollans@gmail.com>
2014-02-08 18:49:18 -0700

    styling changes

--- media/staticfiles/js/site.js
+++ media/staticfiles/js/site.js
@@ -5,12 +5,17 @@ function addDownload(download_url, location) {
         url: "http://192.168.1.76:6800/jsonrpc",
         // The key needs to match your method's input parameter (case-sensitive).
         data: JSON.stringify({'jsonrpc': '2.0', 'id': 'qwer',
-                               'method': 'aria2.addUri',
-                               'params': [[download_url], {'dir': $('#' + location).val()}]}),
+            'method': 'aria2.addUri',
+            'params': [
+                [download_url],
+                {'dir': $('#' + location).val()}
+            ]}),
         contentType: "application/json; charset=utf-8",
         dataType: "json",
-        success: function(data){alert(data);},
-        failure: function(errMsg) {
+        success: function (data) {
+            alert(data);
+        },
+        failure: function (errMsg) {
             alert(errMsg);
         }
     });
@@ -32,7 +37,7 @@ function parse_torrent(filename) {
         $('#id_optional_season').val(parseInt(torrent_data[1]));
         $('#id_optional_episode').val(parseInt(torrent_data[2]));
 
-        Dajaxice.media.search_media(search_callback, $('#search_form').serializeObject() );
+        Dajaxice.media.search_media(search_callback, $('#search_form').serializeObject());
     }
     else {
         var parsed_torrent = filename.match(movie_reg);
@@ -41,16 +46,14 @@ function parse_torrent(filename) {
             $('input[type=radio][value=movie]').prop('checked', true);
             $('#id_search_text').val(jQuery.trim(torrent_data[0].split('.').join(' ').replace(/\[|\)|\]|\(/g, '')));
 
-            Dajaxice.media.search_media(search_callback, $('#search_form').serializeObject() );
+            Dajaxice.media.search_media(search_callback, $('#search_form').serializeObject());
         }
     }
 
 }
 
-function eachRecursive(data, element)
-{
-    for (var data_key in data)
-    {
+function eachRecursive(data, element) {
+    for (var data_key in data) {
         if (typeof data[data_key] == "object" && data[data_key] !== null) {
             // Create a new list group, and then an item that is another list group GROUP-CEPTION!
             // First display the title
@@ -80,7 +83,7 @@ function eachRecursive(data, element)
                 text: data[data_key]
             }).appendTo(element);
             // If this is a title, append the name to to parent.
-            if ( data_key == 'title' ) {
+            if (data_key == 'title') {
                 var parent_anchor = end_element.parent().parent().parent().prev("a.list-group-item");
                 parent_anchor.text(parent_anchor.text() + " - " + data[data_key]);
             }
@@ -119,19 +122,19 @@ function load_media_callback(data) {
     }
 }
 
-function search_callback(data){
+function search_callback(data) {
     if (data.status == 'success') {
         $('#data_loader').empty();
         // Check for empty results
-        if ( Object.keys(data['data']).length == 0 ) {
+        if (Object.keys(data['data']).length == 0) {
             $('#data_loader').html("No matching media. :-(");
         }
         else {
-            $.each(data['data'], function(key, value) {
-            $( "<div>" ).appendTo( '#data_loader' ).addClass( "search_result").attr( "id", "r-" + value['media_id'] );
-            $( "<h3>" ).appendTo( '#r-' + value['media_id'] ).html(value['title']).append( "</h3>" );
-            $( "<div>" ).appendTo( '#r-' + value['media_id'] ).addClass( "sub_element").attr( "id", "s-" + value['media_id']);
-            $( '#s-' + value['media_id'] ).html(value['summary']).append( "</div></div>" );
+            $.each(data['data'], function (key, value) {
+                $("<div>").appendTo('#data_loader').addClass("search_result").attr("id", "r-" + value['media_id']);
+                $("<h3>").appendTo('#r-' + value['media_id']).html(value['title']).append("</h3>");
+                $("<div>").appendTo('#r-' + value['media_id']).addClass("sub_element").attr("id", "s-" + value['media_id']);
+                $('#s-' + value['media_id']).html(value['summary']).append("</div></div>");
             });
         }
     }
@@ -140,95 +143,94 @@ function search_callback(data){
         $('#data_loader').append("<div style='color: red;'>" + data.status + "</div>");
     }
 }
-function torrent_callback(data){
-    if (data.status == 'success'){
+function torrent_callback(data) {
+    if (data.status == 'success') {
         $('#torrent_loader').html("");
         // Check for empty results
-        if ( Object.keys(data['data']).length == 0 ){
+        if (Object.keys(data['data']).length == 0) {
             $('#torrent_loader').html("No search results. :-(");
         }
         else {
-            $.each(data['data'], function(key, value) {
-                $( "<div/>", {
+            $.each(data['data'], function (key, value) {
+                $("<div/>", {
                     class: 'search_result panel panel-info',
                     id: "torrent-" + key
-                }).appendTo( '#torrent_loader' );
-                $( "<div/>", {
+                }).appendTo('#torrent_loader');
+                $("<div/>", {
                     class: 'panel-heading',
                     id: "torrent-" + key + '-heading'
-                }).appendTo( "#torrent-" + key );
-                $( "<h4/>", {
+                }).appendTo("#torrent-" + key);
+                $("<h4/>", {
                     text: value['title']
-                }).appendTo( '#torrent-' + key + '-heading');
-                $( "<div/>", {
+                }).appendTo('#torrent-' + key + '-heading');
+                $("<div/>", {
                     class: "sub_element panel-body",
                     id: "torrent-url-" + key
-                }).appendTo( '#torrent-' + key );
+                }).appendTo('#torrent-' + key);
 
                 // Add a link here to call the ajax request for transmission rpc
-                $( '<button class="btn btn-sm btn-info clicker" id="torrent-b-' + key + '">').appendTo( '#torrent-url-' + key ).html("Click to download").append( "</button></div></div>" );
+                $('<button class="btn btn-sm btn-info clicker" id="torrent-b-' + key + '">').appendTo('#torrent-url-' + key).html("Click to download").append("</button></div></div>");
 
                 // Add a button to call the search to see if it exists
-                $( '<button class="btn btn-sm btn-warning clicker" id="torrent-c-' + key + '">').appendTo( '#torrent-url-' + key ).html("Check if it already exists").append( "</button></div></div>" );
+                $('<button class="btn btn-sm btn-warning clicker" id="torrent-c-' + key + '">').appendTo('#torrent-url-' + key).html("Check if it already exists").append("</button></div></div>");
 
                 // Add the onclick event for transmission RPC
-                $( '#torrent-b-' + key).click(function() {
-                    $( '#torrent-b-' + key).html("Download posted, waiting...");
-                    $( '#torrent-b-' + key).removeClass('btn-info');
-                    $( '#torrent-b-' + key).addClass('btn-warning');
-                    Dajaxice.media.transmission_torrent(transmission_callback, {'url': value['url'], 'id': 'torrent-b-' + key } );
+                $('#torrent-b-' + key).click(function () {
+                    $('#torrent-b-' + key).html("Download posted, waiting...");
+                    $('#torrent-b-' + key).removeClass('btn-info');
+                    $('#torrent-b-' + key).addClass('btn-warning');
+                    Dajaxice.media.transmission_torrent(transmission_callback, {'url': value['url'], 'id': 'torrent-b-' + key });
                 });
 
                 // Add the onclick event for the checking of file existence
-                $('#torrent-c-' + key).click(function() {
+                $('#torrent-c-' + key).click(function () {
                     parse_torrent(value['title'])
                 })
 
             });
         }
     }
-    else
-    {
-        for (message in data.status){
+    else {
+        for (message in data.status) {
             $('#torrent_loader').append("<p><b>" + message + ":</b>" + data.status["message"] + "</p>");
         }
     }
 }
-function transmission_callback(data){
+function transmission_callback(data) {
     if (data.status == 'success') {
-        $( '#' + data.id).removeClass('btn-warning');
-        $( '#' + data.id).addClass('btn-success');
-        $( '#' + data.id).html(data.data)
+        $('#' + data.id).removeClass('btn-warning');
+        $('#' + data.id).addClass('btn-success');
+        $('#' + data.id).html(data.data)
     }
     else {
-        $( '#' + data.id).removeClass('btn-warning');
-        $( '#' + data.id).addClass('btn-danger');
-        $( '#' + data.id).html(data.data)
+        $('#' + data.id).removeClass('btn-warning');
+        $('#' + data.id).addClass('btn-danger');
+        $('#' + data.id).html(data.data)
     }
 }
 
-function files_callback(data){
+function files_callback(data) {
     if (data.status == 'success') {
         var downloadLink = "https://cereal.whatbox.ca/private/files/";
-        $( '#files_loader' ).empty();
-        $.each(data['data'], function(key, value) {
+        $('#files_loader').empty();
+        $.each(data['data'], function (key, value) {
             // Find out if the current div exists, for this key
-            if ( $('#torrent-container-' + key).length == 0 ) {
-                $( '#files_loader').append('<div id="torrent-container-' + key + '"></div>');
+            if ($('#torrent-container-' + key).length == 0) {
+                $('#files_loader').append('<div id="torrent-container-' + key + '"></div>');
                 // Add onclick delete function here
-                $( '#torrent-container-' + key).append('<h3>Torrent ' + key + ' - <button type="button" class="btn btn-xs btn-danger" onclick="delete_torrent(' + key + '); return false;">Delete Torrent and Data</button></h3>');
+                $('#torrent-container-' + key).append('<h3>Torrent ' + key + ' - <button type="button" class="btn btn-xs btn-danger" onclick="delete_torrent(' + key + '); return false;">Delete Torrent and Data</button></h3>');
             }
             // Append each list item to this div
             var length = value.length,
                 element = null;
             for (var i = 0; i < length; i++) {
                 var filesSplit = value[i].split("/");
-                var fileName = filesSplit[filesSplit.length-1];
-                $( '#torrent-container-' + key).append('<div><button type="button" class="btn btn-xs btn-info" onclick="addDownload(' + downloadLink + value[i] + ',"torrent-' + key + '-' + i + '-location");">Parse Torrent</button><a onclick="javascript:addDownload($(this))" id="torrent-' + key + '-' + i + '" href="' + downloadLink + value[i] + '">' + fileName + '</a></div>');
-                $( '#torrent-container-' + key).append('<div><button type="button" class="btn btn-xs btn-info" onclick="parse_torrent(\'' + fileName + '\');">Parse Torrent</button><input id="torrent-' + key + '-' + i + '-location" type="text" class="textinput textInput form-control"></input></div>');
+                var fileName = filesSplit[filesSplit.length - 1];
+                $('#torrent-container-' + key).append('<div><button type="button" class="btn btn-xs btn-info" onclick="addDownload(' + downloadLink + value[i] + ',"torrent-' + key + '-' + i + '-location");">Parse Torrent</button><a id="torrent-' + key + '-' + i + '" href="' + downloadLink + value[i] + '">' + fileName + '</a></div>');
+                $('#torrent-container-' + key).append('<div><button type="button" class="btn btn-xs btn-info" onclick="parse_torrent(\'' + fileName + '\');">Parse Torrent</button><input id="torrent-' + key + '-' + i + '-location" type="text" class="textinput textInput form-control"></input></div>');
             }
         });
-        $("#files_loader > div").tsort("",{attr:"id"});
+        $("#files_loader > div").tsort("", {attr: "id"});
     }
     else {
         alert(data.status);
@@ -236,21 +238,21 @@ function files_callback(data){
 }
 
 function delete_torrent(torrent_id) {
-    Dajaxice.media.delete_torrent(delete_torrent_callback, { 'id': torrent_id } );
-    $( '#torrent-container-' + torrent_id).html('<h3>Torrent ' + torrent_id + '</h3><p>Deleting torrent ' + torrent_id + '...</p>');
+    Dajaxice.media.delete_torrent(delete_torrent_callback, { 'id': torrent_id });
+    $('#torrent-container-' + torrent_id).html('<h3>Torrent ' + torrent_id + '</h3><p>Deleting torrent ' + torrent_id + '...</p>');
 }
 
 function delete_torrent_callback(data) {
     var torrent_id = data['id'];
     if (data.status == 'success') {
-        $( '#torrent-container-' + torrent_id).html('<h3>Torrent ' + torrent_id + '</h3><p>Successfully deleted torrent ' + torrent_id + '.</p>');
+        $('#torrent-container-' + torrent_id).html('<h3>Torrent ' + torrent_id + '</h3><p>Successfully deleted torrent ' + torrent_id + '.</p>');
     }
     else {
         var torrent_error = data['status']
-        $( '#torrent-container-' + torrent_id).html('<h3>Torrent ' + torrent_id + '</h3><p>Failed to delete. Error: ' + torrent_error + '.</p>');
+        $('#torrent-container-' + torrent_id).html('<h3>Torrent ' + torrent_id + '</h3><p>Failed to delete. Error: ' + torrent_error + '.</p>');
     }
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     var timerId = setInterval(refresh_files, 300000);
 });
