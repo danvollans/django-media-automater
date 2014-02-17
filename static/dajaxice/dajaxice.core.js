577769a7341c6a7f17e3b6e4ba506981d1e48919
Daniel Vollans <dan.vollans@gmail.com>
2014-02-17 01:40:44 -0700

    styling changes

--- static/dajaxice/dajaxice.core.js
+++ static/dajaxice/dajaxice.core.js
@@ -9,20 +9,8 @@ var Dajaxice = {
     
     media: {
     
-    load_downloads: function(callback_function, argv, custom_settings){
-        return Dajaxice.call('media.load_downloads', 'POST', callback_function, argv, custom_settings);
-    },
-
-    transfer_file: function(callback_function, argv, custom_settings){
-        return Dajaxice.call('media.transfer_file', 'POST', callback_function, argv, custom_settings);
-    },
-
     refresh_files: function(callback_function, argv, custom_settings){
         return Dajaxice.call('media.refresh_files', 'POST', callback_function, argv, custom_settings);
-    },
-
-    delete_torrent: function(callback_function, argv, custom_settings){
-        return Dajaxice.call('media.delete_torrent', 'POST', callback_function, argv, custom_settings);
     },
 
     search_torrent: function(callback_function, argv, custom_settings){
@@ -37,12 +25,24 @@ var Dajaxice = {
         return Dajaxice.call('media.search_media', 'POST', callback_function, argv, custom_settings);
     },
 
+    purge_downloads: function(callback_function, argv, custom_settings){
+        return Dajaxice.call('media.purge_downloads', 'POST', callback_function, argv, custom_settings);
+    },
+
+    delete_torrent: function(callback_function, argv, custom_settings){
+        return Dajaxice.call('media.delete_torrent', 'POST', callback_function, argv, custom_settings);
+    },
+
     transmission_torrent: function(callback_function, argv, custom_settings){
         return Dajaxice.call('media.transmission_torrent', 'POST', callback_function, argv, custom_settings);
     },
 
-    purge_downloads: function(callback_function, argv, custom_settings){
-        return Dajaxice.call('media.purge_downloads', 'POST', callback_function, argv, custom_settings);
+    load_downloads: function(callback_function, argv, custom_settings){
+        return Dajaxice.call('media.load_downloads', 'POST', callback_function, argv, custom_settings);
+    },
+
+    transfer_file: function(callback_function, argv, custom_settings){
+        return Dajaxice.call('media.transfer_file', 'POST', callback_function, argv, custom_settings);
     }
 
 
--- static/js/site.js
+++ static/js/site.js
@@ -6,6 +6,16 @@ jQuery.expr[":"].Contains = jQuery.expr.createPseudo(function(arg) {
 });
 
 function shrinkById(id) {
+    if (id === 'all') {
+        $('.torrent-collapser').click();
+        if ($('#collapse-all').attr('src') === '/static/img/minus-icon.png') {
+            $('#collapse-all').prop('src', '/static/img/plus.png');
+        }
+        else {
+            $('#collapse-all').prop('src', '/static/img/minus-icon.png');
+        }
+        return;
+    }
     $('#torrent-container-' + id).toggle();
     if ($('#torrent-container-' + id+ '-img').attr('src') === '/static/img/minus-icon.png') {
         $('#torrent-container-' + id + '-img').prop('src', '/static/img/plus.png');
