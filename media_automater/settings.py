b2203c73db35b6b4e14ba0eafb5e1b37dfb90ef4
Daniel Vollans <dan.vollans@gmail.com>
2014-02-28 19:15:34 -0700

    styling changes

--- media_automater/settings.py
+++ media_automater/settings.py
@@ -105,7 +105,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.media',
     'django.core.context_processors.static',
     'django.core.context_processors.request',
-    'django.contrib.messages.context_processors.request',
+    #'django.contrib.messages.context_processors.request',
     'django.contrib.messages.context_processors.messages',
 )
 
