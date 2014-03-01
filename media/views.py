eb519afb20842a2a367af862889a20b7148e7bdf
Daniel Vollans <dan.vollans@gmail.com>
2014-03-01 00:04:49 -0700

    error checking patch to return empty dictionary if the response text is shitty for aria

--- media/views.py
+++ media/views.py
@@ -5,6 +5,7 @@ from media.plex_funcs import *
 from media.forms import *
 from django.core.context_processors import csrf
 from django.contrib.auth.decorators import login_required
+from media_automater.config import TRANSMISSION_DOWNLOAD
 
 
 @login_required()
@@ -13,7 +14,7 @@ def home(request):
     media_header_title = "plex media"
     torrent_header_title = "torrent search"
     files_header_title = "torrent management"
-    template = loader.get_template('index.html')
+    template = loader.get_template('javascript.html')
     content = Context({
 	    'page_title': page_title,
 	    'media_header_title': media_header_title,
@@ -21,6 +22,7 @@ def home(request):
         'files_header_title': files_header_title,
         'search_form': SearchForm(),
         'torrent_form': TorrentForm(),
+        'download_url': TRANSMISSION_DOWNLOAD
 	})
     content.update(csrf(request))
     return HttpResponse(template.render(content))
